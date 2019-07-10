8d5ca8ee988e483b70e2498d1e5614dcf438d56a
Huangyongganguser <52626001+Huangyongganguser@users.noreply.github.com>
2019-07-10 20:02:59 +0800

    Add files via upload
    餐馆

---
+++ Reestaurant.py
@@ -0,0 +1,15 @@
+from nn import Admin
+eric = Admin('eric', 'matthes', 'e_matthes', 'e_matthes@example.com', 'alaska')
+eric.describe_user()
+
+eric.privileges.show_privileges()
+
+print("\nAdding privileges...")
+eric_privileges = [
+    'can reset passwords',
+    'can moderate discussions',
+    'can suspend accounts',
+    ]
+eric.privileges.privileges = eric_privileges
+eric.privileges.show_privileges()
+
